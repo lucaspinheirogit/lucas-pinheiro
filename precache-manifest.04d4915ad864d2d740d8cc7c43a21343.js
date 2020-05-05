ae0efe04c72d385dec00bbdc648e785f8d6b82c7
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-05-05 10:05:21 -0300

    Updates

---
+++ precache-manifest.04d4915ad864d2d740d8cc7c43a21343.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df8f45faf6e97dd1dd40cdab6f0cfd6",
+    "url": "/lucas-pinheiro/index.html"
+  },
+  {
+    "revision": "69ee96366d35045e0d44",
+    "url": "/lucas-pinheiro/static/css/2.2b4f0854.chunk.css"
+  },
+  {
+    "revision": "a2375f7aefc1557a4f8b",
+    "url": "/lucas-pinheiro/static/css/main.e67497e9.chunk.css"
+  },
+  {
+    "revision": "69ee96366d35045e0d44",
+    "url": "/lucas-pinheiro/static/js/2.9ef1792a.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/lucas-pinheiro/static/js/2.9ef1792a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2375f7aefc1557a4f8b",
+    "url": "/lucas-pinheiro/static/js/main.7b8ad942.chunk.js"
+  },
+  {
+    "revision": "92e82604792dc766d84e",
+    "url": "/lucas-pinheiro/static/js/runtime-main.70e4e58a.js"
+  },
+  {
+    "revision": "8e4ce864a9e304efc7322aafb8b3f3f7",
+    "url": "/lucas-pinheiro/static/media/Capturar.8e4ce864.PNG"
+  },
+  {
+    "revision": "daa1b196da384b67bc8939c1c7f4eac5",
+    "url": "/lucas-pinheiro/static/media/algorithm.daa1b196.svg"
+  },
+  {
+    "revision": "ed1a5f401582ddc65919546452859125",
+    "url": "/lucas-pinheiro/static/media/artigo1.ed1a5f40.png"
+  },
+  {
+    "revision": "41b94e7cc735613f870547d30484dfb2",
+    "url": "/lucas-pinheiro/static/media/artigo2.41b94e7c.png"
+  },
+  {
+    "revision": "ca7c5afde7aa162e8f67590f3e8b9d92",
+    "url": "/lucas-pinheiro/static/media/code.ca7c5afd.svg"
+  },
+  {
+    "revision": "dcadc8b7b823c9fb7d0d13b55dc2c8ab",
+    "url": "/lucas-pinheiro/static/media/computer.dcadc8b7.svg"
+  },
+  {
+    "revision": "2a0b2c2fa3efab4b4ac32c39a491850d",
+    "url": "/lucas-pinheiro/static/media/github.2a0b2c2f.svg"
+  },
+  {
+    "revision": "8124a8e110e9c5b73b8fffbf3da69760",
+    "url": "/lucas-pinheiro/static/media/instagram.8124a8e1.svg"
+  },
+  {
+    "revision": "36e4c1981d6f3de93024ebdfed39cf5b",
+    "url": "/lucas-pinheiro/static/media/javascript.36e4c198.png"
+  },
+  {
+    "revision": "843710064cc637e98c046f036453a5f2",
+    "url": "/lucas-pinheiro/static/media/linkedin.84371006.svg"
+  },
+  {
+    "revision": "b54a00781cae277abacd6db4774b1b44",
+    "url": "/lucas-pinheiro/static/media/medium.b54a0078.svg"
+  },
+  {
+    "revision": "5b10b8d3089d2779ca21390daaa82941",
+    "url": "/lucas-pinheiro/static/media/minimal.5b10b8d3.otf"
+  },
+  {
+    "revision": "5195a87db45ec8272720c64add444e71",
+    "url": "/lucas-pinheiro/static/media/mobile.5195a87d.PNG"
+  },
+  {
+    "revision": "3dee0e30fc07251a2b99d6106464dd39",
+    "url": "/lucas-pinheiro/static/media/nodejs.3dee0e30.png"
+  },
+  {
+    "revision": "6f3658eeab43174e7acf40af8b5940cc",
+    "url": "/lucas-pinheiro/static/media/react.6f3658ee.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lucas-pinheiro/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lucas-pinheiro/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lucas-pinheiro/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lucas-pinheiro/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e4ea978a9088ea4b90af85893179e063",
+    "url": "/lucas-pinheiro/static/media/wallpaper.e4ea978a.jpg"
+  },
+  {
+    "revision": "3bac67174a4c71e272d9e8094a14e643",
+    "url": "/lucas-pinheiro/static/media/whatsapp.3bac6717.svg"
+  }
+]);
