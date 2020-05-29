f3e9c935b46822ca14b4a9a5d41f1a699f6f097b
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-05-29 17:52:42 -0300

    remove unused dependencies

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { Suspense, lazy } from 'react'
 
 import Layout from 'components/Layout'
 import I18n from 'components/I18n'
-import { Home } from 'pages'
 
+const Home = lazy(() => import('./pages/Home'))
 const About = lazy(() => import('./pages/About'))
 const Projects = lazy(() => import('./pages/Projects'))
 const Articles = lazy(() => import('./pages/Articles'))
 const Invisible = lazy(() => import('./pages/Invisible'))
 
 const App = () => (
-  <Layout>
-    <Home />
-    <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Layout>
+      <Home />
       <About />
       <Invisible />
       <Projects />
       <Invisible />
       <Articles />
       <I18n />
-    </Suspense>
-  </Layout>
+    </Layout>
+  </Suspense>
 )
 
 export default App
